2db2e40c515cd5d88d6c8d7aae9de9f9907a68a7
Axel Duch <axlduch@gmail.com>
2015-07-02 22:54:51 +0000

    Add logs in init_socket_listeners

--- public/js/init_socket_listeners.js
+++ public/js/init_socket_listeners.js
@@ -1,33 +1,33 @@
 var inputNickname = require('./input_nickname');
+var networkLogger = require('./network_logger');
 
 module.exports = function (socket) {
-   console.log('init_socket_listeners');
-
    socket.on('nickname was asked', function () {
-      console.log('nickname was asked');
-      if (socket.nickname) {
-         socket.emit('nickname was input', socket.nickname);
-      } else {
-         inputNickname(socket);
-      }
+        networkLogger.consume({ message: 'nickname was asked' });
+        if (socket.nickname) {
+            socket.emit('nickname was input', socket.nickname);
+        } else {
+            inputNickname(socket);
+        }
    });
 
    socket.on('nickname rejected', function (duplicateNickname) {
-      delete socket.nickname;
-      inputNickname(socket, duplicateNickname);
+       networkLogger.consume({ message: 'nickname ' + duplicateNickname + ' was rejected'});
+       delete socket.nickname;
+       inputNickname(socket, duplicateNickname);
    });
 
    socket.on('nickname accepted', function (nickname) {
-      console.log('nickname accepted');
-      socket.nickname = nickname;
+       console.log('nickname accepted', nickname);
+       networkLogger.consume({ message: 'nickname ' + nickname + ' accepted' });
+       socket.nickname = nickname;
    });
 
    socket.on('user joined', function (nickname) {
-      console.log('user', nickname, 'joined');
-       alert(nickname + ' has connected');
+      networkLogger.consume({ message: 'user ' + nickname + ' joined' });
    });
 
    socket.on('disconnect', function () {
-      console.log('client disconnected');
+       networkLogger.consume({ message: 'client disconnected' });
    });
 };
