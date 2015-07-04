cba0abe29418a296b9c63429a650b49c0d400244
Axel Duch <axlduch@gmail.com>
2015-07-04 15:36:16 +0000

    Add a delta collecting for match_word.js

---
+++ typemote_engine/words_generator.js
@@ -0,0 +1,21 @@
+var shuffle = require('../shuffle');
+
+function WordsGenerator(params) {
+    params = params || {};
+    var words = params.words || [];
+    var wordCountPerGeneration = params.wordCountPerGeneration || words.length;
+
+    this.words = words;
+    this.wordCountPerGeneration = wordCountPerGeneration;
+}
+
+
+WordsGenerator.prototype.generate = function () {
+    var words = this.words.slice();
+    shuffle(words);
+
+    return words.slice(0, this.wordCountPerGeneration);
+};
+
+
+module.exports = WordsGenerator();
