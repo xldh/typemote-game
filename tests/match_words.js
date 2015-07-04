d8e72b6f8374d6b1a73d39c8778982fd3f971783
Axel Duch <axlduch@gmail.com>
2015-07-04 15:23:28 +0000

    Add tests for match_words.js

---
+++ tests/match_words.js
@@ -0,0 +1,55 @@
+var assert = require('assert');
+var matchWords = require('../typemote_engine/match_words');
+
+describe('matchWords(first, second)', function () {
+    it('should match entirely', function () {
+        var first = 'test';
+        var second = 'test';
+        var expected = {
+            maxRatio: 1,
+            ratio: 1,
+            ratioToMax: 1
+        };
+
+        assert.deepEqual(matchWords(first, second), expected);
+    });
+
+    it('should match partially', function () {
+        var first = 'tes';
+        var second = 'test';
+        var expected = {
+            maxRatio: 0.75,
+            ratio: 0.75,
+            ratioToMax: 1
+        };
+
+        assert.deepEqual(matchWords(first, second), expected);
+    });
+
+    describe('Error handling', function () {
+        it('should throw an error when first is longer than second', function () {
+            var first = 'test';
+            var second = 'tes';
+
+            assert.throws(
+                function () {
+                    matchWords(first, second);
+                },
+                Error,
+                matchWords.ERR_FIRST_TOO_LONG);
+        });
+
+        it('should throw an error when first has a length of 0', function () {
+            var first = '';
+            var second = 'test';
+
+            assert.throws(
+                function () {
+                    matchWords(first, second);
+                },
+                Error,
+                matchWords.ERR_FIRST_NOT_LONG_ENOUGH);
+        });
+    });
+
+});
