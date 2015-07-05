9b9a69703ccf55e9ec358fc15815b3629162ad9e
Axel Duch <axlduch@gmail.com>
2015-07-05 14:39:13 +0000

    Update network_logger template

--- public/js/shared_instances/network_logger.js
+++ public/js/shared_instances/network_logger.js
@@ -1,3 +1,8 @@
 var Logger = require('../logger.js');
+var LogHTMLDisplayer = require('../log_html_displayer');
+var displayer = new LogHTMLDisplayer({
+    container: document.getElementById('network_logger'),
+    templateSelector: '#logger_inner_contents'
+});
 
-module.exports = new Logger();
+module.exports = new Logger(displayer);
