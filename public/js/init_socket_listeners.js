915de7e197aefb09bb02631b9fd9566ed3784261
Axel Duch <axlduch@gmail.com>
2015-07-06 22:11:53 +0000

    Add game picking to the workflow (no rendering yet)

--- public/js/init_socket_listeners.js
+++ public/js/init_socket_listeners.js
@@ -1,15 +1,19 @@
 var networkLogger = require('./shared_instances/network_logger');
 var addNicknameActions = require('./listeners_actions/nickname');
+var addGameActions = require('./listeners_actions/game');
 var addNicknameLogs = require('./listeners_reports/nickname');
 var addUsersLogs = require('./listeners_reports/users');
 var addOwnClientLogs = require('./listeners_reports/own_client');
+var addGameLogs = require('./listeners_reports/game');
 
 module.exports = function (socket) {
     // actions
     addNicknameActions(socket);
+    addGameActions(socket);
 
     // logs
     addNicknameLogs(socket, networkLogger);
+    addGameLogs(socket, networkLogger);
     addUsersLogs(socket, networkLogger);
     addOwnClientLogs(socket, networkLogger);
 };
