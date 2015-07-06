a3f3e6b0cbd788772e7651c303f4fb2f79914efb
Axel Duch <axlduch@gmail.com>
2015-07-06 22:12:52 +0000

    Update server.js, use event_bus and it is getting messy -> needs tidying

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT, process.env.IP);
 var io = require('socket.io')(server);
+var eventBus = require('./shared_instances/event_bus');
 
 app.use('/', express.static('public'));
 
 var nicknames = {};
+
+// FIXME
+// This mess needs to be cleaned up, serious file separation needed around here
+addServerLocalListeners();
 
 io.on('connection', function (socket) {
     console.log('connection');
@@ -40,7 +45,7 @@ function validateNickname(nickname) {
 function tryToRememberSocketNickname(socket, nickname) {
     if (validateNickname(nickname)) {
         rememberSocketNickname(socket, nickname);
-        socket.emit('nickname accepted', nickname);
+        eventBus.emit('nickname accepted', socket, nickname);
         socket.broadcast.emit('user joined', nickname);
     } else {
         console.log('nickname', nickname, 'was rejected');
@@ -60,4 +65,24 @@ function makeSocketAwareOfOthers(socket) {
     for (var nickname in nicknames) {
         socket.emit('user joined', nickname);
     }
+}
+
+function addServerLocalListeners() {
+    eventBus.on('nickname accepted', function (socket, nickname) {
+        var availableGames = getAvailableGames();
+
+        console.log('nickname accepted', availableGames);
+        console.log('asking client to choose a game');
+
+        socket.emit('nickname accepted', nickname);
+        socket.emit('choose a game', availableGames);
+    });
+}
+
+
+function getAvailableGames() {
+    var fs = require('fs');
+    var games = fs.readdirSync('./games');
+
+    return games;
 }
