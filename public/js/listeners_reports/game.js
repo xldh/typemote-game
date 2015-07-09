7ceedb84fbccc893307d02752c6edb9660175559
Axel Duch <axlduch@gmail.com>
2015-07-09 23:12:53 +0000

    Add reporters for game.js

--- public/js/listeners_reports/game.js
+++ public/js/listeners_reports/game.js
@@ -2,6 +2,18 @@ function addGameLogs(socket, logger) {
     socket.on('choose a game', function () {
         logger.consume({ message: 'choose a game' });
     });
+
+    socket.on('your game choice was rejected', function (gameName, gameNames) {
+        logger.consume({
+            message: 'your game choice was rejected (' + gameName + ')'
+        });
+    });
+
+    socket.on('you chose game', function (name, words) {
+        logger.consume({
+            message: 'you chose game ' + name
+        })
+    });
 }
 
 module.exports = addGameLogs;
