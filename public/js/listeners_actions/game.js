de51d791bee448f0f3f6e749b84e5dd088b55aa1
Axel Duch <axlduch@gmail.com>
2015-07-09 23:13:55 +0000

    Handle in a raw way game selection client side

--- public/js/listeners_actions/game.js
+++ public/js/listeners_actions/game.js
@@ -1,7 +1,25 @@
+var eventBus = require('../shared_instances/event_bus');
+
 function addGameActions(socket) {
-    socket.on('choose a game', function (games) {
-        console.log(games);
+    socket.on('choose a game', function (gameNames) {
+        promptGameChoice(socket, gameNames);
     });
+
+    socket.on('your game choice was rejected', function (gameName, gameNames) {
+        promptGameChoice(socket, gameNames);
+    });
+
+    socket.on('you chose game', function (name, words) {
+        eventBus.emit('please init game ' + name, words);
+    });
+}
+
+function promptGameChoice(socket, gameNames) {
+    var gameNumber = prompt(gameNames.map(function (gameName, index) {
+        return (index + 1) + ': ' + gameName;
+    }).join('\n'));
+
+    socket.emit('game chosen', gameNames[gameNumber - 1]);
 }
 
 module.exports = addGameActions;
