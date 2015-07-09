51c03e32727b0f9f5112ec97cc8ad2c4a2750d1c
Axel Duch <axlduch@gmail.com>
2015-07-09 23:15:03 +0000

    Preload available games client side

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,12 @@
+// preload available games
+var availableGames = require('./available_games');
 var initSocket = require('./init_socket');
 var socket = initSocket();
+
+preloadAvailableGames();
+
+function preloadAvailableGames() {
+    availableGames.forEach(function (game) {
+        require('./games/' + game + '/index');
+    });
+}
