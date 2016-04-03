d2ea82eb8a141eb1fc72838b16f99d5833294909
Axel Duch <axlduch@gmail.com>
2016-04-03 02:28:30 +0000

    Remove useless requires

--- games/the_walk/index.js
+++ games/the_walk/index.js
@@ -1,15 +1,11 @@
 var WordsEngine = require('../typemote_engine/words_engine.js');
 var words = require('./words');
-var render = require('./render');
-var update = require('./update');
 var actions = require('./actions');
 
 function initGame() {
     return {
         words: words,
         actions: actions,
-        update: update,
-        render: render,
         states: {
             left: false,
             right: false,
