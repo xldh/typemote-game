dd2a094113436701aa14331c23717441afaea18d
Axel Duch <axlduch@gmail.com>
2016-04-03 00:47:48 +0000

    Update the game "the walk" to work with the game instanciator

--- public/js/games/the_walk/index.js
+++ public/js/games/the_walk/index.js
@@ -1,7 +1,36 @@
 var eventBus = require('../../shared_instances/event_bus');
+var render = require('./render');
 
 eventBus.on('please init game the_walk', function (words) {
     console.log('"The walk" game init was asked politely', words);
 });
 
-module.exports = true;
+function Game(words) {
+    if (Game.instance) {
+        return Game.instance;
+    }
+
+    this.hero = {
+        x: 0,
+        y: 0
+    };
+
+    Game.instance = this;
+}
+
+Game.instance = null;
+
+Game.prototype.run = function () {
+    var game = this;
+
+    window.requestAnimationFrame(function () {
+        game.update();
+    });
+};
+
+Game.prototype.update = function () {
+    var game = Game.instance;
+    window.requestAnimationFrame(game.update);
+};
+
+module.exports = Game;
