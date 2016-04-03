7f8d2c547fe5f3e382e333b4dd88fc6094e2c9d2
Axel Duch <axlduch@gmail.com>
2016-04-03 14:22:38 +0000

    Handle view input to build word

--- public/js/main.js
+++ public/js/main.js
@@ -5,8 +5,47 @@ var socket = initSocket();
 
 preloadAvailableGames();
 
+
 function preloadAvailableGames() {
     availableGames.forEach(function (game) {
         require('./games/' + game + '/index');
     });
+}
+
+
+var $ = require('jquery');
+var eventBus = require('./shared_instances/event_bus');
+var TypemoteInputHandler = require('./typemote_input_handler');
+
+var inputHandler = new TypemoteInputHandler();
+registerKeyboardEvents();
+
+
+function registerKeyboardEvents() {
+    var $input = $('<input type="text">');
+    $input.css({ opacity: 0 });
+
+    $(document.body).append($input);
+    $input.focus();
+
+    $(document).click(function () {
+        $input.focus();
+    });
+
+    $input.keypress(function (event) {
+
+        if (event.which === 13) {
+            eventBus.emit('end of word was typed');
+            return;
+        }
+
+        setTimeout(function () {
+            var char = $input.val();
+
+            if (char) {
+                eventBus.emit('char was typed', char);
+                $input.val('');
+            }
+        }, 0);
+    })
 }
