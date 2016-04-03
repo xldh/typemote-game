d8b6f9fc63c0019ada2134ffd990c848710a9a53
Axel Duch <axlduch@gmail.com>
2016-04-03 13:39:53 +0000

    Add jquery through bower

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,19 @@
+var webpack = require("webpack");
+var BowerWebpackPlugin = require('bower-webpack-plugin');
+
 module.exports = {
     entry: "./public/js/main.js",
     output: {
         path: __dirname + '/public/js/',
         filename: "bundle.js"
-    }
+    },
+    plugins: [
+        new BowerWebpackPlugin({
+            excludes: /.*\.less/
+        }),
+        new webpack.ProvidePlugin({
+            $:      "jquery",
+            jQuery: "jquery"
+        })
+    ]
 };
