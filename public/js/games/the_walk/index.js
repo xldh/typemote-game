93c4a613a5aaac54de2aa10d56d6db4c57633909
Axel Duch <axlduch@gmail.com>
2016-04-03 02:30:47 +0000

    First rendering (testing so far) for the walk game

--- public/js/games/the_walk/index.js
+++ public/js/games/the_walk/index.js
@@ -1,5 +1,6 @@
 var eventBus = require('../../shared_instances/event_bus');
-var render = require('./render');
+var context2d = require('../../shared_instances/context2d');
+var Drawer = require('../../Drawer');
 
 eventBus.on('please init game the_walk', function (words) {
     console.log('"The walk" game init was asked politely', words);
@@ -11,12 +12,20 @@ function Game(words) {
     }
 
     this.hero = {
+        width: 0.1,
+        height: 0.1,
         x: 0,
         y: 0
     };
 
+    this.hero.x = 0.1;
+    this.hero.y = 0.1;
+    this.drawer = new Drawer(context2d);
+
+
     Game.instance = this;
 }
+
 
 Game.instance = null;
 
@@ -25,12 +34,21 @@ Game.prototype.run = function () {
 
     window.requestAnimationFrame(function () {
         game.update();
+        game.render();
+        game.run();
     });
 };
 
-Game.prototype.update = function () {
-    var game = Game.instance;
-    window.requestAnimationFrame(game.update);
+
+Game.prototype.render = function () {
+    this.drawer.clear();
+    this.drawer.drawRect(this.hero);
 };
+
+
+Game.prototype.update = function () {
+    this.hero.x += 0.0001;
+};
+
 
 module.exports = Game;
